3c63dbc858dd51594d366b6fdaf46f7b00bb9b31
Tharushika-Ekanayaka <ekanaya-ps20017@stu.kln.ac.lk>
2024-11-19 14:31:35 +0530

    thirdcommit_student_repository

--- CRUD_application/src/main/java/com/example/CRUD_application/CRUD_application/repository/StudentRepository.java
+++ CRUD_application/src/main/java/com/example/CRUD_application/CRUD_application/repository/StudentRepository.java
@@ -3,6 +3,9 @@ package com.example.CRUD_application.CRUD_application.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.CRUD_application.CRUD_application.model.Student;
+import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
     List<Student> findByYearsOfEnrollment(int years);
