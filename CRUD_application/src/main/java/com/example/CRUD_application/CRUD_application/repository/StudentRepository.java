467f566eab32088aa386ea06dbe80fb6d9c4e55f
Tharushika-Ekanayaka <ekanaya-ps20017@stu.kln.ac.lk>
2024-11-19 14:12:48 +0530

    Update StudentRepository.java

--- CRUD_application/src/main/java/com/example/CRUD_application/CRUD_application/repository/StudentRepository.java
+++ CRUD_application/src/main/java/com/example/CRUD_application/CRUD_application/repository/StudentRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.CRUD_application.CRUD_application.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
-    
+    List<Student> findByYearsOfEnrollment(int years);
+   @Query("SELECT department FROM Student WHERE id = ?1")
+    String findDepartmentById(long id);
+    @Modifying
+    @Query("DELETE FROM Student WHERE yearsOfEnrollment = ?1")
+void deleteStudentByYearsOfEnrollment(int yearsOfEnrollment); 
 }
